6c8cbad278911c950777c4cbd4775cf49555bbe4
Kristina Morozova <christina.vadi@gmail.com>
2023-04-18 00:26:14 +0600

    fix: carbon dating

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -18,7 +18,7 @@ const HALF_LIFE_PERIOD = 5730;
  *
  */
 function dateSample(sampleActivity) {
-  if (typeof sampleActivity === 'string' && +sampleActivity < MODERN_ACTIVITY ) {
+  if (typeof sampleActivity === 'string' && +sampleActivity < MODERN_ACTIVITY && +sampleActivity > 0) {
     return Math.ceil(Math.log(MODERN_ACTIVITY / sampleActivity) / (0.693 / HALF_LIFE_PERIOD));
   } else {
     return false;
